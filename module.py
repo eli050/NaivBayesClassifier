93bcea9880bb78bd7f9d4e5697507874068f450c
eli <lykkh78@gmail.com>
2025-07-08 19:09:01 +0300

    adding base wights

---
+++ module.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from pandas import DataFrame
+from pprint import pprint
+
+df = pd.read_csv("train.csv")
+df = df.dropna()
+df = df.drop(columns=['Name','PassengerId'])
+
+dict_wights = dict()
+for target in df["Survived"].unique():
+    dict_wights[
+        target
+    ] = dict()
+    for column in df.columns[1:]:
+        dict_wights[
+            target
+        ][
+            column
+        ] = dict()
+        for value in df[column].unique():
+            dict_wights[target][column][value] = 0
+        for value in df[column][df["Survived"] == target]:
+            dict_wights[target][column][value] += 1
+
+
+pprint(dict_wights)
+
+
+
+
+
+
+class BaseWights:
+    @staticmethod
+    def get_wights(df:DataFrame):
+        pass
