9930e459fb7ae5f40bfc6c74007cc6ec42979c9b
eli <lykkh78@gmail.com>
2025-07-09 15:21:56 +0300

    adding create model class

---
+++ services/model_trainer/create_model.py
@@ -0,0 +1,47 @@
+from pprint import pprint
+
+import pandas as pd
+
+from services.data_cleaner.cleaner_service import CleanData
+from services.data_reader.read_csv import ReadCSV
+
+
+class CreateModel:
+    def __init__(self,df:pd.DataFrame,target_column:str):
+        self.df = df
+        self.target_column = target_column
+    def get_dict_wights(self):
+        dict_wights = dict()
+        for target in self.df[self.target_column].unique():
+            dict_wights[
+                target
+            ] = dict()
+            for column in self.df.columns[1:]:
+                dict_wights[target][column] = dict()
+                values =  self.df[column].unique()
+                vals_per_trg = self.df[column][(self.df[self.target_column] == target)]
+                # print(vals_per_trg)
+                flag = set(values) == set(vals_per_trg.unique())
+                # print(flag)
+                if flag:
+                    for value in values:
+                        dict_wights[target][column][value] = \
+                            (self.df[column][(self.df[self.target_column] == target)
+                                        & (self.df[column] == value)].shape[0]) / vals_per_trg.value_counts().sum()
+                else:
+                    for value in values:
+                        dict_wights[target][column][value] = \
+                            (self.df[column][(self.df[self.target_column] == target)
+                                        & (self.df[column] == value)].shape[0] + 1 ) / (vals_per_trg.value_counts().sum() + values.size)
+        return dict_wights
+
+
+
+
+
+
+rc = ReadCSV("C:\\users\\home\\PycharmProjects\\NaiveBayesClassifier\\Data\\train.csv")
+df_trg = CleanData().clean_df(rc.get_data())
+cm = CreateModel(df_trg[0],df_trg[1])
+pprint(cm.get_dict_wights())
+# cm.get_dict_wights()
